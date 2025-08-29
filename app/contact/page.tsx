480025ac20523601e369dfef568af5057fa64857
Adarsh Gupta <adarshgupta7ag@gmail.com>
2025-08-29 18:52:22 +0530

    minor change

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,7 @@
 export default function Contact () {
 
     console.log("Is this server or client component?");
+    console.log("This is client component")
     return (
         <div>Contact Page</div>
     )
